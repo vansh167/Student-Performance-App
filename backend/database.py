fdee35dd30f52c11442d0aa2818b32302f810ee8
Vansh <vanshdoc7@gmail.com>
2026-01-20 12:42:16 +0530

    0010

--- backend/database.py
+++ backend/database.py
@@ -6,8 +6,12 @@ load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DB_NAME")
-COLLECTION = os.getenv("COLLECTION")
+
+STUDENTS_COLLECTION = os.getenv("COLLECTION")  # students collection name
+USERS_COLLECTION = os.getenv("USERS_COLLECTION", "users")  # default users
 
 client = MongoClient(MONGO_URL)
 db = client[DB_NAME]
-students_collection = db[COLLECTION]
+
+students_collection = db[STUDENTS_COLLECTION]
+users_collection = db[USERS_COLLECTION]

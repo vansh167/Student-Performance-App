d2789d9da524546a1e155e2823e211ef51e94a5d
Vansh <vanshdoc7@gmail.com>
2026-01-24 00:41:17 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "./src/context/ThemeContext.jsx";
+import { ThemeProvider } from "../src/context/ThemeContext.jsx";
 
 const theme = localStorage.getItem("theme");
 

5aa0b1b0340ec6adc718fe74d56ce3db6468e86a
Vansh <vanshdoc7@gmail.com>
2026-01-24 18:09:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ThemeProvider } from "./Component/ThemeContext";
-const theme = localStorage.getItem("theme");
 
-if (!theme) {
-  localStorage.setItem("theme", "dark");
-  document.body.setAttribute("data-theme", "dark");
-} else {
-  document.body.setAttribute("data-theme", theme);
-}
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <ThemeProvider>
+      
       <App />
-      </ThemeProvider>
+    
   </React.StrictMode>
 );

b07dbcab9710f7ada86a49658085676a976613f3
Vansh <vanshdoc7@gmail.com>
2026-01-24 13:17:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
-import { useContext } from "react";
-import { ThemeContext } from "./Component/ThemeContext.jsx";
-
-import Recommendations from "./Pages/Recommendations.jsx";
-import Profile from "./Pages/Profile.jsx";
-import Navbar from "./Component/Navbar/Navbar.jsx";
-import Home from "./Component/HomePage/Home.jsx";
-import About from "./Pages/About.jsx";
-import Signup from "./Pages/Signup.jsx";
-import Login from "./Pages/Login.jsx";
-import Ranking from "./Pages/Ranking.jsx";
-import Welcome from "./Pages/Welcome.jsx";
-import StudentAnalytics from "./Pages/StudentAnalytics.jsx";
-import AdminPanel from "./Pages/AdminPanel.jsx";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";  
+import { useContext } from "react";  
+import { ThemeContext } from "./Component/ThemeContext.jsx";  
+  
+import Recommendations from "./Pages/Recommendations.jsx";  
+import Profile from "./Pages/Profile.jsx";  
+import Navbar from "./Component/Navbar/Navbar.jsx";  
+import Home from "./Component/HomePage/Home.jsx";  
+import About from "./Pages/About.jsx";  
+import Signup from "./Pages/Signup.jsx";  
+import Login from "./Pages/Login.jsx";  
+import Ranking from "./Pages/Ranking.jsx";  
+import Welcome from "./Pages/Welcome.jsx";  
+import StudentAnalytics from "./Pages/StudentAnalytics.jsx";  
+import AdminPanel from "./Pages/AdminPanel.jsx";  
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" replace />;

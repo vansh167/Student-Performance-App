ac5b8f5d6537b15faea81d8d4b557a5a6e6795cf
dilpreetheckedup <dilpreetheckedup@gmail.com>
2026-07-21 21:57:52 +0530

    fix 2nd

--- backend/database.py
+++ backend/database.py
@@ -5,16 +5,7 @@ from pymongo import MongoClient
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME")
-COLLECTION = os.getenv("COLLECTION")
 
-# 1️⃣ Connect MongoDB
+print("MONGO_URL =", MONGO_URL)
+
 client = MongoClient(MONGO_URL)
-
-# 2️⃣ Create database
-db = client[DB_NAME]
-
-# 3️⃣ Collections
-students_collection = db[COLLECTION]
-users_collection = db["users"]
-resources_collection = db["resources"]   # ✅ now db exists
